d6ad129634e9608e7cbf3a5301a41be2d9155115
amuh387 <151036055+amuh387@users.noreply.github.com>
2024-02-09 11:23:58 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,9 @@
+function calcular() {
+       let a, b
+       a=parseInt(prompt("Inserta un número"));
+       b=parseInt(prompt("Inserta otro número"));
+       
+    alert("El primer número es : " + (a) + "\nEl segundo número es : " + (b) + "\nLa suma es: " + (a+b)  + "\nLa resta es: " + (a-b) + "\nLa multiplicacion es: " + 
+    (a*b) + "\nLa división es: " + (a/b) + " \nEl resto es: " + (a%b));
+
+}
