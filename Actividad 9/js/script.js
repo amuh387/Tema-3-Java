cbd729d1dc3e0cdca28b4fd2a2ced81c4512a40a
amuh387 <151036055+amuh387@users.noreply.github.com>
2024-02-22 14:04:13 +0100

    Update script.js

--- Actividad 9/js/script.js
+++ Actividad 9/js/script.js
@@ -1,6 +1,6 @@
 function factorial() {
     let a;
-    a=parseInt(document.getElementById("a"). value, 0);
+    a=parseInt(document.getElementById("a").value, 0);
     let resultado= 1;
     let iteracion=1;
 
