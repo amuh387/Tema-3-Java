3328c28c1a4ac292d3d0e75a4f6e1509b125da45
amuh387 <151036055+amuh387@users.noreply.github.com>
2024-02-22 19:26:05 +0100

    Add files via upload

---
+++ Actividad 10/js/script.js
@@ -0,0 +1,7 @@
+function Calificar() {
+    let alumnos = ["Pepe", "Jorge", "Juan", "Andrés"];
+    let notas = [];
+    for (let i = 0; i < 4; i++) {
+        let nota = (prompt("Ingrese la nota para " + alumnos[i] + ":"));
+    }
+}
